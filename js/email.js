0e27ecfe414a0423222bbdc5ba22420da8425905
connectmeweb <166869418+connectmeweb@users.noreply.github.com>
2024-04-13 18:23:49 +0100

    Update email.js

--- js/email.js
+++ js/email.js
@@ -1,1 +1,10 @@
+let Email = document.getElementById("emailid")
+let Message = document.getElementById("message")
 
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    console.log(user)
+  } else {
+    
+  }
+});
