f5130cae7711badde7cfd9c9b1086029cd3ce884
connectmeweb <166869418+connectmeweb@users.noreply.github.com>
2024-04-13 17:04:53 +0100

    Add files via upload

---
+++ js/firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyATmLsbYrOCm_EAsv5XkQ14C8kFx6qqsyw",
+    authDomain: "smileychat-62e16.firebaseapp.com",
+    projectId: "smileychat-62e16",
+    storageBucket: "smileychat-62e16.appspot.com",
+    messagingSenderId: "197396289724",
+    appId: "1:197396289724:web:e53f63f238a70c397358b9",
+    measurementId: "G-DW9QG98Q6J"
+  };
+
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
