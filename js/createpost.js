fd381d413f129cfd675fdf0de159a60ed24942bf
connectmeweb <166869418+connectmeweb@users.noreply.github.com>
2024-04-14 17:31:25 +0100

    Update createpost.js

--- js/createpost.js
+++ js/createpost.js
@@ -1,1 +1,9 @@
+let postValue = document.getElementById("textarea")
+let progressdiv = document.getElementById("progressdiv")
+let progressbar = document.getElementById("progressbar")
+let currentUser=""
+let url = ""
+let fileType = ""
+let done = document.getElementById("done")
+let uid
 
