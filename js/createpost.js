363196339acae5f936ac41678fef7a3e47ea9ea1
connectmeweb <166869418+connectmeweb@users.noreply.github.com>
2024-04-14 17:35:33 +0100

    Update createpost.js

--- js/createpost.js
+++ js/createpost.js
@@ -7,3 +7,31 @@ let fileType = ""
 let done = document.getElementById("done")
 let uid
 
+firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      if (user.emailVerified) {
+        // home
+        setTimeout(() => {
+          uid = user.uid
+        }, 1000);
+      } else {
+        // email verification
+        setTimeout(() => {
+          window.location.assign("./pages/email.html");
+        }, 1000);
+      }
+    } else {
+      // login
+      setTimeout(() => {
+        window.location.assign("./pages/login.html");
+      }, 1000);
+    }
+  });
+
+firebase.auth().onAuthStateChanged((user) => {
+  currentUser = user
+})
+
+let uploading = (event) =>{
+  
+}
