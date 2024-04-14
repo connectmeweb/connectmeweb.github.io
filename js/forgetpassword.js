b988bb0ea6fc3cbad41a553d40a94334c1183851
connectmeweb <166869418+connectmeweb@users.noreply.github.com>
2024-04-14 11:06:44 +0100

    Update forgetpassword.js

--- js/forgetpassword.js
+++ js/forgetpassword.js
@@ -6,5 +6,17 @@ let email = document.getElementById("email")
 let Message = document.getElementById("message")
 
 const reset = () =>{
-  
+  if(email.value === "") {
+    Message.innerHTML = "Email Address is required"
+    Message.style.color = "red"
+    email.focus()
+  }
+  firebase.auth().sendPasswordResetEmail(email)
+  .then(() => {
+    window.location.assign("/pages/emailVerification.html")
+  })
+  .catch((error) => {
+    Message.innerHTML = error.message;
+    Message.style.color = "red"
+  });
 }
