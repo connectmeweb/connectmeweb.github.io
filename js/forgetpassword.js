1815c12556b7944ae17308c6671da13710c61246
connectmeweb <166869418+connectmeweb@users.noreply.github.com>
2024-04-14 11:01:27 +0100

    Update forgetpassword.js

--- js/forgetpassword.js
+++ js/forgetpassword.js
@@ -1,3 +1,10 @@
 const back =()=>{
   window.location.assign("/pages/login.html")
 }
+
+let email = document.getElementById("email")
+let Message = document.getElementById("message")
+
+const reset = () =>{
+  
+}
