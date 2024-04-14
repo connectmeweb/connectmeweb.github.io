00b8567454fbfff07bf82da1a8769cee76cb5523
connectmeweb <166869418+connectmeweb@users.noreply.github.com>
2024-04-14 12:02:35 +0100

    Update loader.js

--- js/loader.js
+++ js/loader.js
@@ -3,19 +3,19 @@ firebase.auth().onAuthStateChanged((user) => {
       if (user.emailVerified) {
         // home
         setTimeout(() => {
-          window.location.assign("/pages/home.html");
+          window.location.assign("./pages/home.html");
         }, 1000);
       } else {
         // email verification
         setTimeout(() => {
-          window.location.assign("/pages/email.html");
+          window.location.assign("./pages/email.html");
         }, 1000);
       }
     } else {
       // login
       setTimeout(() => {
-        window.location.assign("/pages/login.html");
+        window.location.assign("./pages/login.html");
       }, 1000);
     }
   });
-  
+}
